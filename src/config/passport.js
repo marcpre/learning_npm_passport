584a8cfe23fbc392899438335240aa8091b0cdab
marcpre <marcus.presich@gmail.com>
2017-11-12 11:58:58 +0100

    running eslint

--- src/config/passport.js
+++ src/config/passport.js
@@ -2,23 +2,23 @@ const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const serviceAuth = require('../service/auth')
 
-//module.exports = () => {
-  // serializeUser determines, which data of the user object should be stored in the session.
-  // see: https://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize
-  passport.serializeUser((user, done) => {
-    done(null, user.id)
-  })
+// module.exports = () => {
+// serializeUser determines, which data of the user object should be stored in the session.
+// see: https://stackoverflow.com/questions/27637609/understanding-passport-serialize-deserialize
+passport.serializeUser((user, done) => {
+  done(null, user.id)
+})
 
-  passport.deserializeUser(async (id, done) => {
-    const user = await serviceAuth.findById(id)
-    done(null, user)
-  })
+passport.deserializeUser(async (id, done) => {
+  const user = await serviceAuth.findById(id)
+  done(null, user)
+})
 
-  // Sign in with username and Password
-  passport.use('local', new LocalStrategy({
-    usernameField: 'username',
-  }, async(username, password, done) => {
-    const user = await serviceAuth.signin(username, password)
-    done(null, user)
-  }))
-//}
+// Sign in with username and Password
+passport.use('local', new LocalStrategy({
+  usernameField: 'username',
+}, async(username, password, done) => {
+  const user = await serviceAuth.signin(username, password)
+  done(null, user)
+}))
+// }
